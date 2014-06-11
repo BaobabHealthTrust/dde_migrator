1b5ca54a7e8810421ef3fabda1ab053e844fe745
Blessings <blessings.mhango@gmail.com>
2014-06-11 13:06:58 +0200

    person relationship

--- app/models/dde_legacy_national_ids.rb
+++ app/models/dde_legacy_national_ids.rb
@@ -1,5 +1,5 @@
 class DdeLegacyNationalIds < ActiveRecord::Base
   self.table_name = 'legacy_national_ids'
-  default_scope where(:voided => false)
-  belongs_to :ddeperson
+  default_scope {where(voided: 0)}
+  belongs_to :person, :class_name => 'DdePerson'
 end

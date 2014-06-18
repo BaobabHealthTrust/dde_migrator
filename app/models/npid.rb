58813609f307c5453de22d93d8f80f109e7eb83c
Blessings <blessings.mhango@gmail.com>
2014-06-18 09:31:27 +0200

    added new production views

--- app/models/npid.rb
+++ app/models/npid.rb
@@ -17,8 +17,42 @@ class Npid < CouchRest::Model::Base
   timestamps!
   
   design do
-    view :by__id
-    view :by_national_id 
+    if Rails.env.downcase == "production"
+        # NOTE: all occurences of "doc['type'] == 'npid'" are using lowercase type
+        # which is different from the way CouchRest::Model creates its type field
+        # which is normally "Npid" instead. This is due to the way initialisation
+        # data was created which helps bring a difference between manually
+        # generated data and that from the application. This is the case mainly
+        # because data in this group of documents is expected to be pre-generated 
+        # externally and just consumed by the application.
+        # Site views
+       
+        view :by__id,
+            :map => "function(doc) {
+                if ((doc['type'] == 'npid') && (doc['_id'] != null)) {
+                  emit(doc['_id'], 1);
+                }
+              }"   
+        view :by__national_id,
+            :map => "function(doc) {
+                if ((doc['type'] == 'npid') && (doc['_national_id'] != null)) {
+                  emit(doc['_national_id'], 1);
+                }
+              }" 
+        view :all,
+            :map => "function(doc) {
+              if (doc['type'] == 'npid') {
+                emit(doc._id, null);
+              }
+            }"
+    else 
+        # NOTE: this set of views is created specifically for "development" and 
+        # "test" environments only and not to be used in "production" mode.
+        
+        view :by__id
+        view :by__national_id
+        
+    end
   end
 
 end

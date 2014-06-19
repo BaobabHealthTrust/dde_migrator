836b5a747a868513c6e3f684a11919a3a8e0a8d1
Blessings <blessings.mhango@gmail.com>
2014-06-19 09:31:34 +0200

    removed not required methods

--- app/models/dde_national_patient_identifier.rb
+++ app/models/dde_national_patient_identifier.rb
@@ -20,13 +20,7 @@ class DdeNationalPatientIdentifier < ActiveRecord::Base
     end
   end
   
-  min = SITE_CONFIG[:base_npid].to_i
-  max = min + SITE_CONFIG[:npid_range].to_i
-  @@possible_ids ||= (min..max).map
   
-  @@generated_ids ||= NationalPatientIdentifier.select('decimal_num').map(&:decimal_num) rescue nil
-  @@last_id = nil
-  @@last_id ||= NationalPatientIdentifier.last.id rescue nil
 
   def self.find_or_create_from_attributes(attrs, options = {:update => false})
     if attrs['value']
@@ -68,39 +62,6 @@ class DdeNationalPatientIdentifier < ActiveRecord::Base
 EOF
 
     ActiveRecord::Base.connection().execute(sql)
-  end
-
-  protected
-
-  def self.next_id_val(version, site_code=nil, last_number = nil)
-    if version == 3
-      raise ArgumentError, %q(site_code (2nd arg) is mandatory for version 3 ids) if site_code.blank?
-      npid_version  = '1'
-      npid_prefix   = "P#{npid_version}#{site_code.rjust(3, '0')}"
-      last_number ||= self.first(:conditions => ['left(value, 5) = ?', npid_prefix], :order => 'value DESC').try(:value) || nil
-      last_number ||= SITE_CONFIG[:base_npid] || '0'
-      
-      next_number = (last_number.to_s[5..-2].to_i + 1).to_s.rjust(7, '0')
-      national_id_without_check_digit = "#{npid_prefix}#{next_number}"
-      
-      "#{national_id_without_check_digit}#{NationalPatientId.check_digit(national_id_without_check_digit[1..-1])}"
-    elsif version == 4
-      NationalPatientId.new(NationalPatientIdentifier.next_random_num).value
-    else
-      raise ArgumentError, %Q(Unsupported version "#{version}". Should be 3 or 4)
-    end
-  end
-  
-  def self.next_random_num
-    if @@last_id
-      @@generated_ids += NationalPatientIdentifier.select('id').where(['id > ?', @@last_id]).map(&:id)
-    else
-      @@generated_ids = NationalPatientIdentifier.select('id').map(&:id)
-    end
-    @@last_id = NationalPatientIdentifier.select('id').last.id rescue nil
-    available_ids = @@possible_ids - @@generated_ids
-    
-    available_ids[rand(available_ids.length)]
   end
 
 end

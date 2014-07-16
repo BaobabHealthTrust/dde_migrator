edd75aa0a1df8d7787a33c6f0f557c8131a4dac0
Blessings <blessings.mhango@gmail.com>
2014-07-16 15:23:23 +0200

    removed relationships

--- app/models/dde_national_patient_identifier_original.rb
+++ app/models/dde_national_patient_identifier_original.rb
@@ -2,34 +2,11 @@ class DdeNationalPatientIdentifierOriginal < ActiveRecord::Base
   establish_connection :originalmaster
   self.table_name = 'national_patient_identifiers'
   default_scope { where("decimal_num IS NOT NULL", voided: 0 ) }
-  belongs_to :person, :class_name => 'DdePerson'
-  belongs_to :assigner, :class_name => 'DdeUser'
-  belongs_to :assigner_site, :class_name => 'DdeSite', :foreign_key => 'assigner_site_id'
-
+  
   validates_presence_of :value, :assigner_site_id
 
   # don't allow more than one ID to be assigned to any person
   validates_uniqueness_of :person_id, :allow_nil => true
-
-  # create the decimal equivalent of the Id value if it has not yet been set
-  before_save do |npid|
-    if npid.decimal_num.blank?
-      num = NationalPatientId.to_decimal(npid.value, 30) / 10
-      npid.decimal_num = num
-    end
-  end
-  
-  
-
-  def self.find_or_create_from_attributes(attrs, options = {:update => false})
-    if attrs['value']
-      self.find_or_initialize_by_value(attrs['value']).tap do |npid|
-        npid.update_attributes(attrs) if npid.new_record? or options[:update]
-      end
-    else
-      raise ArgumentError, %q(expected attrs hash to have key named 'value')
-    end
-  end
 
   def remote_attributes
     { 'npid' => {

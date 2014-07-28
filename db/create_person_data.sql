ef63e05515c54ce82e778d9ec3c7c9f2d5995705
Blessings <blessings.mhango@gmail.com>
2014-07-28 13:32:45 +0200

    added timestamps

--- db/create_person_data.sql
+++ db/create_person_data.sql
@@ -15,5 +15,7 @@ CREATE TABLE `person_data` (
   `home_ta` varchar(40) DEFAULT NULL,
   `home_district` varchar(40) DEFAULT NULL,
   `legacy_ids` varchar(100) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
